3064ef5fd07838f82be0c3345c943d83614304fd
steadyfish <dhrumins68@gmail.com>
2018-09-25 06:51:25 -0400

    updated feature_engg_modelling.py
    has first cut of the preprocessing pipeline

--- python/feature_engg_modelling.py
+++ python/feature_engg_modelling.py
@@ -15,6 +15,7 @@ from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
+import sklearn_pandas as skp
 
 cdir = path.abspath("..")
 col_names = ["s1", "c1", "s2", "c2", "s3", "c3", "s4", "c4", "s5", "c5", "hand"]
@@ -28,60 +29,73 @@ col_types = {"s1": cat, "c1": num,
              "hand": cat}
 d_in = pd.read_csv(cdir + "\\data\\poker-hand-training-true.data", 
                    names = col_names, dtype = col_types)
-# precomputed features
-d_in_pre = pd.read_feather(cdir + "\\data\\poker-hand-training-true-precomputed.data")
+d_in = pd.read_feather(cdir +  "\\data\\poker-hand-training-true.data")
+# create features "sklearn" way
 
-d_in1 = d_in_pre[d_in_pre.hand.isin(['2', '3', '4', '6'])]
-d_in1 = d_in_pre
+df_prep = skp.DataFrameMapper([(['s1', 's2', 's3', 's4', 's5'], my_trans_func(), {'alias': 'suit_match'}),
+                               ()], 
+                              input_df = True, df_out = True, default = None)
+t = df_prep.fit_transform(d_in.copy())
+#d_in['suit_match'] = (d_in['s1'] == d_in['s2']  == d_in['s3'] == d_in['s4'] == d_in['s5'])
+d_in['suit_match'] = (d_in['s1'] == d_in['s2']) & (d_in['s1'] == d_in['s3']) & (d_in['s1'] == d_in['s4']) & (d_in['s1'] == d_in['s5'])
+d_in.groupby(['suit_match']).count()
+d_in.groupby(['hand', 'suit_match']).count()
 
-y = d_in1.hand
-X = d_in1.loc[:, 'suit_match':'has_straight'] # produces a copy
+d_in['suit_match1'] = d_in[['s1', 's2', 's3', 's4', 's5']].apply(get_repeats, criteria = 5, axis = 1)
+d_in[['hand','suit_match','suit_match1']].groupby(['suit_match', 'suit_match1']).count()
 
-estimators = [('clf', LogisticRegression())]
-pipe = ppl.Pipeline(estimators)
-#estimators = [('dummify', ppr.OneHotEncoder()), ('clf', SVC())]
-#pipe = ppl.Pipeline(estimators)
-# same as make_pipeline(ppr.OneHotEncoder(), SVC())
-pipe.fit(X = X, y = y)
+# d_record = row in the dataframe
+# criteria = 2, 3, 4, 5
+def get_repeats(d_record, criteria):
+    
+    d_r1 = d_record.groupby(d_record).count()
+    d_r2 = d_r1[d_r1 == criteria]
+    return(d_r2.shape[0])
 
-pipe.get_params() # model tuning parameters
-pipe.named_steps['clf'].intercept_
-pipe.named_steps['clf'].coef_
+# example code - https://www.kaggle.com/gautham11/building-predictive-models-with-sklearn-pipelines
+#from sklearn.preprocessing import FunctionTransformer
 
-y_pred = pipe.predict(X)
-y_pred_prob = pipe.predict_proba(X)
-metrics.confusion_matrix(y, y_pred)
-metrics.brier_score_loss( y, y_pred_prob[:, 1])
+def Comparator(criteria=1):
+#def ColumnsEqualityChecker(result_column='equality_col', inverse=False):
+    def get_repeats_outer(d_in, criteria):
+        
+        def get_repeats(d_record, criteria):
+#            print(type(d_record))
+#            print(d_record.shape)
+            d_r1 = d_record.groupby(d_record).count()
+            d_r2 = d_r1[d_r1 == criteria]
+            return(d_r2.shape[0])
+        a = d_in[['s1', 's2', 's3', 's4', 's5']].apply(get_repeats, criteria = criteria, axis = 1)
+        return(a)
+#        return pd.DataFrame(eq.values.astype(int), columns=[result_column])
+    return ppr.FunctionTransformer(
+        get_repeats_outer,
+        validate=False,
+        kw_args={'criteria': criteria}
+#        kw_args={'result_column': result_column, 'inverse': inverse}
+    )
 
-# temp code only
-y_pred1 = pd.Series(y_pred)
-pd.DataFrame([y, y_pred])
-yy = pd.concat([y, y_pred1], axis = 1)#.reset_index()
-yy.columns = ['hand', 'pred_hand']
-yy['temp'] = 1
-#pd.pivot_table(yy, index = ['hand', 'pred_hand'])
-yy.groupby(['hand', 'pred_hand']).count()
+engineered_feature_pipeline1 = skp.DataFrameMapper([
+        (['s1', 's2', 's3', 's4', 's5'], Comparator(criteria = 5), {'alias': 'suit_match'}),
+        (['c1', 'c2', 'c3', 'c4', 'c5'], Comparator(criteria = 2), {'alias': 'no_pairs'}),
+        (['c1', 'c2', 'c3', 'c4', 'c5'], Comparator(criteria = 3), {'alias': 'has_triplet'}),
+        (['c1', 'c2', 'c3', 'c4', 'c5'], Comparator(criteria = 4), {'alias': 'has_quartet'})], 
+    input_df=True, df_out=True, default = None)
 
-## temporary
-a = np.arange(0, 100, 2)
-b = np.arange(100, 0, -2)
-err = 2*np.random.rand(50)
-c = np.exp(-(.02*a + .03*b + err))
-d = 1/(1 + c)
-yy = np.where(d > d.mean(), 1, 0)
-xx = pd.DataFrame({'a': a, 'b':b})
-# same as xx = pd.concat([a, b])
+temp = d_in[d_in['hand']=='2']
+engineered_feature_pipeline1.fit_transform(temp).head()
 
-m = LogisticRegression()
-m.fit(X = xx, y = yy)
-m.coef_
-m.intercept_
-yy_pred = m.predict(xx)
-yy_pred_prob = m.predict_proba(xx)
-metrics.confusion_matrix(yy, yy_pred)
-metrics.brier_score_loss(yy, yy_pred_prob[:, 1])
+engineered_feature_pipeline2 = skp.DataFrameMapper([
+        (['c1', 'c2', 'c3', 'c4', 'c5'], Comparator(criteria = 3), {'alias': 'has_triplet'})], 
+    input_df=True, df_out=True, default = None)
 
-#yy_out = pd.DataFrame({'act': yy, 'pred': m.predict(xx), 'dummy': np.arange(50)})
-#yy_out.groupby(['act', 'pred']).count()
+engineered_feature_pipeline3 = skp.DataFrameMapper([
+        (['c1', 'c2', 'c3', 'c4', 'c5'], Comparator(criteria = 4), {'alias': 'has_quartet'})], 
+    input_df=True, df_out=True, default = None)
 
+features_pipeline = make_union(engineered_feature_pipeline1, 
+                      engineered_feature_pipeline2, 
+                      engineered_feature_pipeline3)
 
+temp = d_in[d_in['hand']=='2']
+features_pipeline.fit_transform(temp).head()
